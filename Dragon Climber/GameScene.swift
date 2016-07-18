6569e63799295aac0873ebc575737df28e1e5abf
leyishi <aliceshi@Seans-MacBook-Pro.local>
2016-07-18 10:15:32 -0700

    scrolls world, beginning of add rock func

--- Dragon Climber/GameScene.swift
+++ Dragon Climber/GameScene.swift
@@ -9,37 +9,46 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    /* Scroll Speed */
+    let scrollSpeed: CGFloat = 160
+    
+    /* Fixed Delta */
+    let fixedDelta: CFTimeInterval = 1.0/60.0 /* 60 FPS */
+    
+    
+    /* UI Connections */
+    var Rock: SKSpriteNode!
+    var scrollLayer: SKNode!
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        /* Reference to UI Connections */
+        Rock = self.childNodeWithName("Rock") as! SKSpriteNode
+        scrollLayer = self.childNodeWithName("scrollLayer")
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        /* Process World Scrolling */
+        scrollWorld()
     }
+    
+    func scrollWorld() {
+        scrollLayer.position.y -= scrollSpeed * CGFloat(fixedDelta)
+    }
+    
+    func createRocks() {
+        /* creates randomized rocks */
+        
+        let randomRockpos = arc4random()
 }
